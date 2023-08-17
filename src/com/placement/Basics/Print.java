9f5114a0621a1ee9c651eee0e3c0d2706f633898
ayush-10702 <ayushprakhar12@gmail.com>
2023-08-18 00:02:10 +0530

    New Codes Uploaded

---
+++ src/com/placement/Basics/Print.java
@@ -0,0 +1,9 @@
+package com.placement.Basics;
+
+import java.sql.SQLOutput;
+
+public class Print{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
